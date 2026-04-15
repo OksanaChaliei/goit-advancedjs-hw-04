ea7cb8c67107b3b5c45af72902c5118b7d02673d
Oksana Chaliei <dondaksana13@gmail.com>
2026-04-15 08:37:07 +0200

    image search with pagination, loader and lightbox

---
+++ src/main.js
@@ -0,0 +1,99 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import { getImagesByQuery } from './js/pixabay-api';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+  showLoadMoreButton,
+  hideLoadMoreButton,
+} from './js/render-functions';
+
+const form = document.querySelector('.form');
+const loadMoreBtn = document.querySelector('.load-more');
+
+let page = 1;
+let query = '';
+let totalHits = 0;
+
+form.addEventListener('submit', async e => {
+  e.preventDefault();
+
+  query = e.target.elements['search-text'].value.trim();
+
+  if (!query) return;
+
+  page = 1;
+  clearGallery();
+  hideLoadMoreButton();
+  showLoader();
+
+  try {
+    const data = await getImagesByQuery(query, page);
+    totalHits = data.totalHits;
+
+    if (data.hits.length === 0) {
+      iziToast.error({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+      });
+      return;
+    }
+
+    createGallery(data.hits);
+
+    const totalPages = Math.ceil(totalHits / 15);
+
+    if (totalPages > 1) {
+      showLoadMoreButton();
+    } else {
+      hideLoadMoreButton();
+      iziToast.info({
+        message: "We're sorry, but you've reached the end of search results.",
+      });
+    }
+  } catch (error) {
+    iziToast.error({
+      message: 'Error loading images.',
+    });
+  } finally {
+    hideLoader();
+  }
+});
+
+loadMoreBtn.addEventListener('click', async () => {
+  page += 1;
+  showLoader();
+
+  try {
+    const data = await getImagesByQuery(query, page);
+
+    createGallery(data.hits);
+
+    const totalPages = Math.ceil(totalHits / 15);
+
+    if (page >= totalPages) {
+      hideLoadMoreButton();
+
+      iziToast.info({
+        message: "We're sorry, but you've reached the end of search results.",
+      });
+    }
+
+    const card = document.querySelector('.gallery-item');
+    const cardHeight = card.getBoundingClientRect().height;
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+  } catch (error) {
+    iziToast.error({
+      message: 'Error loading more images.',
+    });
+  } finally {
+    hideLoader();
+  }
+});
